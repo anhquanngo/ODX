8cbb3dbae374c2acc338b4c68292f36f7b2543d6
anh-quan <anhquan31299@gmail.com>
2026-06-10 16:27:14 +0700

    Disable miniglog in CMake configuration for Ceres to prevent conflicts with COLMAP logging. This change ensures compatibility by using the system glog instead.

--- SuperBuild/cmake/External-Ceres.cmake
+++ SuperBuild/cmake/External-Ceres.cmake
@@ -19,8 +19,8 @@ ExternalProject_Add(${_proj_name}
     -DBUILD_EXAMPLES=OFF
     -DBUILD_TESTING=OFF
     -DUSE_CUDA=OFF
-    -DMINIGLOG=ON
-    -DMINIGLOG_MAX_LOG_LEVEL=-100
+    # Use system glog (libgoogle-glog-dev); miniglog breaks COLMAP (FLAGS_logtostderr).
+    -DMINIGLOG=OFF
     -DCMAKE_INSTALL_PREFIX:PATH=${SB_INSTALL_DIR}
     ${WIN32_CMAKE_ARGS}
   #--Build step-----------------
